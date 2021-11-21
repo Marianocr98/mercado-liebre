8f970efccd8132a439164487fb5a2d0b05b7fe8b
Mariano Cespedez <marianocespedez1998@gmail.com>
2021-11-21 20:20:41 -0300

    Mariano Cespedez - Subiendo mi Mercado Liebre terminado

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ app.listen(process.env.PORT || 3000, () => console.log(`Servidor funcionando`));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/home.html");
 })
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html");
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+})
